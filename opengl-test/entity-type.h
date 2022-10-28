88751103377c2fb57d259d3b7bea1c2e24b70b34
Zeimd <jv.korkala@gmail.com>
2022-10-28 21:15:45 +0300

    Add files for entity type to project.

---
+++ opengl-test/entity-type.h
@@ -0,0 +1,17 @@
+/*****************************************************************************
+*
+* entity-type.h
+*
+* By Jari Korkala 10/2022
+*
+*****************************************************************************/
+
+#ifndef CENGINE_ENTITY_TYPE_H
+#define CENGINE_ENTITY_TYPE_H
+
+namespace CEngine
+{
+
+}
+
+#endif
