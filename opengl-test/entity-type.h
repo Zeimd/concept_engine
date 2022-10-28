a70a5f7d72cf91d52220d06fe44819ebce5a212b
Zeimd <jv.korkala@gmail.com>
2022-10-28 21:55:34 +0300

    Add minimum data fields to entity type class.

--- opengl-test/entity-type.h
+++ opengl-test/entity-type.h
@@ -9,9 +9,31 @@
 #ifndef CENGINE_ENTITY_TYPE_H
 #define CENGINE_ENTITY_TYPE_H
 
+#include <unordered_map>
+#include <memory>
+
+#include <ceng.h>
+
 namespace CEngine
 {
+	class Component;
 
+	class EntityType
+	{
+	public:
+
+		// Components that are shared between all instances
+		// Example: mesh, animation definitions, scripts
+		std::unordered_map<Ceng::StringUtf8, std::shared_ptr<Component>> sharedComponents;
+
+		// Default components that must be deep copied when creating new entity
+		// Example: animation state, script state
+		std::unordered_map<Ceng::StringUtf8, std::shared_ptr<Component>> defaultComponents;
+
+		// Components whose initialization must be provided when instance is created.
+		// Example: position, 
+		std::vector<Ceng::StringUtf8> requiredComponents;
+	};
 }
 
 #endif
