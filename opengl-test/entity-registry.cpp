1aa84b480c1246cbd173f5c85b0fe824fcbbe349
Zeimd <jv.korkala@gmail.com>
2022-10-30 22:48:51 +0200

    Initial implementation of entity registry.

--- opengl-test/entity-registry.cpp
+++ opengl-test/entity-registry.cpp
@@ -7,6 +7,10 @@
 *****************************************************************************/
 
 #include "entity-registry.h"
+#include "entity.h"
+#include "entity-type.h"
+#include "component.h"
+#include "component-factory.h"
 
 using namespace CEngine;
 
@@ -20,12 +24,178 @@ EntityRegistry::~EntityRegistry()
 
 }
 
-EngineResult::value EntityRegistry::AddComponentFactory()
+EngineResult::value EntityRegistry::GetEntityInstance(Ceng::StringUtf8& name,
+	json& initialValues, std::shared_ptr<Entity>& output)
 {
+	output = nullptr;
+
+	auto& typeIter = entityTypes.find(name);
+
+	if (typeIter == entityTypes.end())
+	{
+		return EngineResult::fail;
+	}
+
+	auto entity = std::make_shared<Entity>();
+
+	for (auto& iter : typeIter->second->sharedComponents)
+	{
+		entity->AddComponent(iter.first, iter.second);
+	}
+
+	// Deep copy default components
+	for (auto& iter : typeIter->second->defaultComponents)
+	{
+		entity->AddComponent(iter.first, std::make_shared<Component>(*(iter.second)));
+	}
+
+	EngineResult::value eresult;
+
+	// Add required components
+	for (auto& iter = initialValues.begin(); iter != initialValues.end(); ++iter)
+	{
+		auto& factory = componentFactories.find(iter.key());
+
+		if (factory == componentFactories.end())
+		{
+			return EngineResult::fail;
+		}
+
+		std::shared_ptr<Component> component;
+
+		eresult = factory->second->GetInstance(iter.value(), component);
+
+		if (eresult != EngineResult::ok)
+		{
+			return eresult;
+		}
+
+		eresult = entity->AddComponent(iter.key(), component);
+
+		if (eresult != EngineResult::ok)
+		{
+			return eresult;
+		}
+	}
+
 	return EngineResult::ok;
 }
 
-EngineResult::value EntityRegistry::AddEntityType()
+EngineResult::value EntityRegistry::AddComponentFactory(Ceng::StringUtf8& name,
+	std::shared_ptr<ComponentFactory>& factory)
 {
+	if (componentFactories.find(name) != componentFactories.end())
+	{
+		return EngineResult::fail;
+	}
+
+	componentFactories[name] = factory;
+
 	return EngineResult::ok;
 }
+
+EngineResult::value EntityRegistry::RemoveComponentFactory(Ceng::StringUtf8& name)
+{
+	auto& iter = componentFactories.find(name);
+
+	if (iter == componentFactories.end())
+	{
+		return EngineResult::fail;
+	}
+
+	componentFactories.erase(iter);
+
+	return EngineResult::ok;
+}
+
+EngineResult::value EntityRegistry::RemoveComponentFactory(std::shared_ptr<ComponentFactory>& factory)
+{
+	/*
+	for (auto& iter : componentFactories)
+	{
+		if (iter.second == factory)
+		{
+			componentFactories.erase(iter);
+
+			return EngineResult::ok;
+		}
+	}
+	*/
+
+	return EngineResult::fail;
+}
+
+EngineResult::value EntityRegistry::ReplaceComponentFactory(Ceng::StringUtf8& name,
+	std::shared_ptr<ComponentFactory>& factory)
+{
+	auto& iter = componentFactories.find(name);
+
+	if (iter == componentFactories.end())
+	{
+		return EngineResult::fail;
+	}
+
+	componentFactories[name] = factory;
+
+	return EngineResult::ok;	
+}
+
+EngineResult::value EntityRegistry::AddEntityType(Ceng::StringUtf8& name, 
+	std::shared_ptr<EntityType>& type)
+{
+	if (entityTypes.find(name) != entityTypes.end())
+	{
+		return EngineResult::fail;
+	}
+
+	entityTypes[name] = type;
+
+	return EngineResult::ok;
+}
+
+EngineResult::value EntityRegistry::RemoveEntityType(Ceng::StringUtf8& name)
+{
+	auto& iter = entityTypes.find(name);
+
+	if (iter == entityTypes.end())
+	{
+		return EngineResult::fail;
+	}
+
+	entityTypes.erase(iter);
+
+	return EngineResult::ok;
+}
+
+EngineResult::value EntityRegistry::RemoveEntityType(std::shared_ptr<EntityType>& type)
+{
+	/*
+	for (auto& iter : entityTypes)
+	{
+		if (iter.second == type)
+		{
+			entityTypes.erase(iter);
+
+			return EngineResult::ok;
+		}
+	}
+	*/
+
+	return EngineResult::fail;
+}
+
+EngineResult::value EntityRegistry::ReplaceEntityType(Ceng::StringUtf8& name, 
+	std::shared_ptr<EntityType>& type)
+{
+	auto& iter = entityTypes.find(name);
+
+	if (iter == entityTypes.end())
+	{
+		return EngineResult::fail;
+	}
+
+	entityTypes[name] = type;
+
+	return EngineResult::ok;
+}
+
--- opengl-test/entity-registry.h
+++ opengl-test/entity-registry.h
@@ -14,17 +14,20 @@
 
 #include <ceng.h>
 
+#include "json.h"
+
 #include "engine-result.h"
 
 namespace CEngine
 {
+	class Entity;
 	class EntityType;
 	class ComponentFactory;
 
 	class EntityRegistry
 	{
 	public:
-		std::unordered_map < Ceng::StringUtf8, std::shared_ptr<EntityType>> entityTypes;
+		std::unordered_map<Ceng::StringUtf8, std::shared_ptr<EntityType>> entityTypes;
 
 		std::unordered_map<Ceng::StringUtf8, std::shared_ptr<ComponentFactory>> componentFactories;
 
@@ -33,9 +36,28 @@ namespace CEngine
 		EntityRegistry();
 		~EntityRegistry();
 
-		EngineResult::value AddComponentFactory();
+		EngineResult::value AddComponentFactory(Ceng::StringUtf8& name, 
+			std::shared_ptr<ComponentFactory>& factory);
 
-		EngineResult::value AddEntityType();
+		EngineResult::value RemoveComponentFactory(Ceng::StringUtf8& name);
+
+		EngineResult::value RemoveComponentFactory(std::shared_ptr<ComponentFactory>& factory);
+
+		EngineResult::value ReplaceComponentFactory(Ceng::StringUtf8& name,
+			std::shared_ptr<ComponentFactory>& factory);
+
+		EngineResult::value AddEntityType(Ceng::StringUtf8& name, 
+			std::shared_ptr<EntityType>& type);
+
+		EngineResult::value RemoveEntityType(Ceng::StringUtf8& name);
+
+		EngineResult::value RemoveEntityType(std::shared_ptr<EntityType>& type);
+
+		EngineResult::value ReplaceEntityType(Ceng::StringUtf8& name, 
+			std::shared_ptr<EntityType>& type);
+
+		EngineResult::value GetEntityInstance(Ceng::StringUtf8& name, 
+			json& initialValues, std::shared_ptr<Entity>& output);
 
 	};
 }
