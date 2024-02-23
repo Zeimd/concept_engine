2bd85b40778514767d2f01b7a77412e740954d52
Zeimd <jv.korkala@gmail.com>
2024-02-23 09:33:23 +0200

    Move helper definitions to header.

--- inc/env-map.h
+++ inc/env-map.h
@@ -20,6 +20,63 @@ namespace CEngine
 
 	void RayDir(const Ceng::UINT32 u, const Ceng::UINT32 v, const Ceng::CubemapFace::value face,
 		const Ceng::FLOAT32 cubeWidth, Vec3 *out_dir);
+
+	class CubemapData
+	{
+	public:
+		Ceng::UINT32 width;
+		Ceng::UINT32 faceSize;
+		Vec4* faceData[6];
+
+		CubemapData()
+		{
+			faceSize = 0;
+			for (int i = 0; i < 6; i++)
+			{
+				faceData[i] = nullptr;
+			}
+		}
+
+		CubemapData(Ceng::UINT32 width) : width(width)
+		{
+			faceSize = 4 * width * width;
+
+			for (int i = 0; i < 6; i++)
+			{
+				faceData[i] = (Vec4*)malloc(faceSize * sizeof(Vec4));
+			}
+		}
+
+		~CubemapData()
+		{
+			for (int i = 0; i < 6; i++)
+			{
+				if (faceData[i] != nullptr)
+				{
+					free(faceData[i]);
+				}
+			}
+		}
+	};
+
+	struct IrradianceThreadCommon
+	{
+		Ceng::FLOAT32 invDestWidth;
+		Ceng::Matrix4* faceMatrix;
+		Ceng::Matrix4* faceTranspose;
+		Vec4* solidAngleRayDir;
+		CubemapData* sourceMap;
+		CubemapData* destMap;
+	};
+
+	struct TaskData_v3
+	{
+		Vec3* normal;
+		Vec3 output;
+		Ceng::FLOAT32* dest;
+		Ceng::UINT32 sourceFace;
+	};
+
 }
 
 #endif
